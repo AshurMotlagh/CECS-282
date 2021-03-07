9d1c0860989c43e5bc0f03e7cadf7bd1355900c0
Ashur Motlagh <ashurmotlagh@gmail.com>
2021-03-07 13:22:26 -0800

    Created New Project

---
+++ Projects/StudentList/cmake-build-debug/CMakeFiles/StudentList.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StudentList.dir/main.cpp.obj"
+  "StudentList.exe"
+  "StudentList.exe.manifest"
+  "StudentList.pdb"
+  "libStudentList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/StudentList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
