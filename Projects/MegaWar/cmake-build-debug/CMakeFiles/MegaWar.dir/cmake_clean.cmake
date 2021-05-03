4073f39a466653404c8bb61adb68ba5ca9bbe9dc
Ashur Motlagh <ashurmotlagh@gmail.com>
2021-05-02 22:40:50 -0700

    added MegaWar

---
+++ Projects/MegaWar/cmake-build-debug/CMakeFiles/MegaWar.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MegaWar.dir/main.cpp.obj"
+  "MegaWar.exe"
+  "MegaWar.exe.manifest"
+  "MegaWar.pdb"
+  "libMegaWar.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MegaWar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
